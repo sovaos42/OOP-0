94949a68f9f1476ec68249eff77f38999cce863f
sovaos42 <sovaos42@gmail.com>
2022-09-22 13:05:35 +0700

    data

--- locallibrary/locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/locallibrary/settings.py
@@ -77,23 +77,10 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.mysql',
-
-        'NAME': 'DB_NAME',
-
-        'USER': 'DB_USER',
-
-        'PASSWORD': 'DB_PASSWORD',
-
-        'HOST': 'localhost',
-
-        'PORT': '3306',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
 
 # Password validation
